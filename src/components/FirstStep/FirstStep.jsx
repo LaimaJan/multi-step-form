f8e37d72a3135b68ae6c3de57fdb72a9fc1513e7
LaimaJan <lm.lingyte@gmail.com>
2023-10-30 19:01:06 +0200

    working on step one

--- src/components/FirstStep/FirstStep.jsx
+++ src/components/FirstStep/FirstStep.jsx
@@ -8,27 +8,29 @@ export default function FirstStep() {
 				<FormStepsSideBar className={'first-step'} />
 				<div className="form-buttons-container">
 					<div className="personal-info-form">
-						<div className="info-form-container">
-							<div className="info">
-								<p className="heading">Personal info</p>
-								<p className="additional-info">
-									Please provide your name, email address, and phone number.
-								</p>
-							</div>
-							<div className="form-labels-inputs-container">
-								<label>
-									Name:
-									<input type="text" name="name" />
-								</label>
-								<label>
-									Email Address:
-									<input type="email" name="email" />
-								</label>
-								<label>
-									Phone Number:
-									<input type="tel" name="phone" />
-								</label>
-							</div>
+						<div className="info">
+							<p className="heading">Personal info</p>
+							<p className="additional-info">
+								Please provide your name, email address, and phone number.
+							</p>
+						</div>
+						<div className="form-labels-inputs-container">
+							<label>
+								Name
+								<input type="text" name="name" placeholder="Name" />
+							</label>
+							<label>
+								Email Address
+								<input type="email" name="email" placeholder="Email" />
+							</label>
+							<label>
+								Phone Number
+								<input
+									type="tel"
+									name="phone"
+									placeholder="e.g. +1234 567 890"
+								/>
+							</label>
 						</div>
 					</div>
 					<div className="form-buttons">
--- src/components/FormStepsSideBar/FormStepsSideBar.jsx
+++ src/components/FormStepsSideBar/FormStepsSideBar.jsx
@@ -2,14 +2,52 @@ import './FormStepsSideBar.css';
 import PropTypes from 'prop-types';
 import SideBarBackgroundDesktop from '../../assets/images/bg-sidebar-desktop.svg';
 
-export default function FormStepsSide({ className }) {
+export default function FormStepsSideBar({ className }) {
 	return (
 		<div className={`form-steps-side ${className}`}>
 			<img src={SideBarBackgroundDesktop} alt="side-bar-background-img" />
+			<div className="steps-card-container">
+				<div className="first-step-container steps-container">
+					<div className="step-number">
+						<p>1</p>
+					</div>
+					<div className="steps-info">
+						<p className="step">Step 1</p>
+						<p className="step-additional-info">Your Info</p>
+					</div>
+				</div>
+				<div className="second-step-container steps-container">
+					<div className="step-number">
+						<p>2</p>
+					</div>
+					<div className="steps-info">
+						<p className="step">Step 2</p>
+						<p className="step-additional-info">Select Plan</p>
+					</div>
+				</div>
+				<div className="third-step-container steps-container">
+					<div className="step-number">
+						<p>3</p>
+					</div>
+					<div className="steps-info">
+						<p className="step">Step 3</p>
+						<p className="step-additional-info">Add-ons</p>
+					</div>
+				</div>
+				<div className="fourth-step-container steps-container">
+					<div className="step-number">
+						<p>4</p>
+					</div>
+					<div className="steps-info">
+						<p className="step">Step 4</p>
+						<p className="step-additional-info">Summary</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
 
-FormStepsSide.propTypes = {
+FormStepsSideBar.propTypes = {
 	className: PropTypes.string,
 };
